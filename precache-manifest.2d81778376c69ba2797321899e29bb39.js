2288c9ae4121719182d6487cbbdd87ff8569a953
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2019-12-01 13:20:40 +0100

    Updates

---
+++ precache-manifest.2d81778376c69ba2797321899e29bb39.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae7f8ddedfb485cc3e8ef7e3dd842f0",
+    "url": "/grefsenkollen-opp/index.html"
+  },
+  {
+    "revision": "73ada66842b724db4d6a",
+    "url": "/grefsenkollen-opp/static/js/2.0a5ce978.chunk.js"
+  },
+  {
+    "revision": "ae5ed8e2dec77fe8cb01",
+    "url": "/grefsenkollen-opp/static/js/main.d9064aea.chunk.js"
+  },
+  {
+    "revision": "bf9dc82cb8f5c771419c",
+    "url": "/grefsenkollen-opp/static/js/runtime-main.2255fd28.js"
+  }
+]);
