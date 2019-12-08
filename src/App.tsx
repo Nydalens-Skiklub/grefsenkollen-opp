19018e1e608c3c8665f74e52a300e646a50cc28b
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2019-12-08 14:12:39 +0100

    Add scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { HashRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter, useLocation } from "react-router-dom";
 
 import { Navbar } from "./navbar/Navbar";
 import { Main } from "./main/Main";
 import { Footer } from "./footer/Footer";
 
+const ScrollTopTop: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const App: React.FC = () => {
   return (
     <HashRouter>
+      <ScrollTopTop />
       <Navbar />
       <Main />
       <Footer />
